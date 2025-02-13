9326bba804f8b641ed61a346a5b4d8206a33a803
clarence112 <clarencepacelli@gmail.com>
2025-02-12 19:03:01 -0500

    important to remember to chain super() through all of these

--- Scripts/Players/Snaily.gd
+++ Scripts/Players/Snaily.gd
@@ -3,9 +3,11 @@ extends Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	super()
 	$JsonSprite2D.action = "an_action"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	super(delta)
 	pass
